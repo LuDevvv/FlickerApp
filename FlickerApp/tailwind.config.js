6af5abef804060b2093915b787ca81843e4326cd
Luis Garcia L <ludevgarcialouis@gmail.com>
2023-10-19 13:13:56 -0400

    structure folders + config⚡

---
+++ FlickerApp/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./Pages/**/*.cshtml", "./Views/**/*.cshtml"],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
